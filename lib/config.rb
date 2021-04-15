7a52383b2b33e44da376e4d4befcbda7e74a3d6e
masahito1997 <velonica.gale2227@gmail.com>
2021-04-15 21:47:48 +0900

    add config singleton class

---
+++ lib/config.rb
@@ -0,0 +1,22 @@
+require 'singleton'
+
+class Config
+  include Singleton
+
+  COLUMN = %i[image_dir images contrast_min contrast_max page_number threshold_min threshold_max particle_size_min particle_size_max]
+
+  attr_accessor *COLUMN
+
+  def set?(column_name)
+    return false unless COLUMN.include? column_name.to_sym
+
+    !send(column_name).nil?
+  end
+
+  def method_missing(name, *args)
+    return super if name =~ /\Aset_(.+?)\?\z/
+    return super unless COLUMN.include? Regexp.last_matches[0]
+
+    set? Regexp.last_matches[0]
+  end
+end
