fec05bd14ca7f33d6564d68e28095511e4bdd184
masahito1997 <velonica.gale2227@gmail.com>
2021-04-16 18:12:46 +0900

    refactor: use GridBagLayoutHelper and ConfigTextField

--- services/contrast_range_service.rb
+++ services/contrast_range_service.rb
@@ -1,82 +1,28 @@
 # @AppService appService
 require_relative "#{$appService.getApp.getBaseDirectory}/plugins/JRuby/imagej.rb"
 
-java_import "javax.swing.JPanel"
-java_import "javax.swing.JLable"
-java_import "javax.swing.JTextField"
-java_import "javax.swing.BoxLayout"
-java_import "javax.swing.event.DocumentListener"
+require_relative "../lib/config_text_field.rb"
+require_relative "../lib/grid_bag_layout_helper.rb"
 
-java_import "java.awt.event.ActionListener"
+java_import "javax.swing.JLabel"
 
-class ContrastRangeService
+class ContrastRangeService < BaseService
+  include GridBagLayoutHelper
+
   MAX_TEXT_FIELD_COUNT = 10
 
   def initialize(frame)
+    super()
     @frame = frame
   end
 
   def call!
-    panel = build_panel
-
-    panel.add build_label('コントラスト')
-
-    min_contrast_textfield = build_text_field(:contrast_min, 'min')
-    panel.add min_contrast_textfield
-
-    panel.add build_label(' - ')
-
-    max_contrast_textfield = build_text_field(:contrast_max, 'max')
-    panel.add max_contrast_textfield
+    add_component_with_constraints(0, 0, 1, 2) { JLabel.new 'Contrast' }
+    add_component_with_constraints(1, 0, 1, 1) { JLabel.new '最小値' }
+    add_component_with_constraints(2, 0, 1, 1) { ConfigTextField.new :contrast_min, '', MAX_TEXT_FIELD_COUNT }
+    add_component_with_constraints(1, 1, 1, 1) { JLabel.new '最大値' }
+    add_component_with_constraints(2, 1, 1, 1) { ConfigTextField.new :contrast_max, '', MAX_TEXT_FIELD_COUNT }
 
     @frame.get_content_pane.add panel
-  end
-
-  private
-
-  def build_panel
-    panel = JPanel.new
-    panel.set_layout(BoxLayout.new(panel, BoxLayout.X_AXIS))
-    panel
-  end
-
-  def build_label(label_value)
-    JLable.new(label_value)
-  end
-
-  def build_text_field(config_attribute, place_holder = '')
-    text_field = JTextField.new place_holder, MAX_TEXT_FIELD_COUNT
-
-    action = ContrastDocumentListener.new(text_field, config_attribute)
-    text_field.get_document.add_document_listener(action)
-    text_field
-  end
-
-  class ContrastDocumentListener
-    include DocumentListener
-
-    def initialize(text_field, config_attribute)
-      @text_field = text_field
-      @config_attribute = config_attribute
-      @config = Config.instance
-    end
-
-    def changed_update
-      self.contrast_value = text_field.get_text
-    end
-
-    def remove_update
-      self.contrast_value = text_field.get_text
-    end
-
-    def insert_update
-      self.contrast_value = text_field.get_text
-    end
-
-    private
-
-    def contrast_value=(value)
-      @config.send("#{@config_attribute}=", value)
-    end
   end
 end
