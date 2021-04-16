614d7123a77e86642047a6965d06a0f7476f4314
masahito1997 <velonica.gale2227@gmail.com>
2021-04-16 18:02:05 +0900

    implement grid_bag_layout_helper

---
+++ lib/grid_bag_layout_helper.rb
@@ -0,0 +1,32 @@
+# @AppService appService
+require_relative "#{$appService.getApp.getBaseDirectory}/plugins/JRuby/imagej.rb"
+
+java_import "javax.swing.JPanel"
+
+java_import "java.awt.GridBagLayout"
+java_import "java.awt.GridBagConstraints"
+
+module GridBagLayoutHelper
+  def layout
+    @layout ||= GridBagLayout.new
+  end
+
+  def panel
+    @panel ||= @panel = JPanel.new layout
+  end
+
+  def constraints
+    @constraints ||= GridBagConstraints.new
+  end
+
+  def add_component_with_constraints(gridx, gridy, gridwidth, gridheight, &block)
+    constraints.gridx = gridx
+    constraints.gridy = gridy
+    constraints.gridwidth = gridwidth
+    constraints.gridheight = gridheight
+
+    component = block.call
+    layout.set_constraints component, constraints
+    panel.add component
+  end
+end
