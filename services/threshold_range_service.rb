8d8920346c1505d2f46d6e64f22f5e57f726be13
masahito1997 <velonica.gale2227@gmail.com>
2021-04-16 18:03:34 +0900

    refactor: use GridBagLayoutHelper

--- services/threshold_range_service.rb
+++ services/threshold_range_service.rb
@@ -2,6 +2,7 @@
 require_relative "#{$appService.getApp.getBaseDirectory}/plugins/JRuby/imagej.rb"
 
 require_relative "../lib/config_text_field.rb"
+require_relative "../lib/grid_bag_layout_helper.rb"
 
 java_import "javax.swing.JPanel"
 java_import "javax.swing.JLable"
@@ -12,17 +13,13 @@ java_import "java.awt.GridBagConstraints"
 java_import "java.awt.Insets"
 
 class ThresholdRangeService < BaseService
+  include GridBagLayoutHelper
+
   MAX_TEXT_FIELD_COUNT = 10
 
   def initialize(frame)
     super
     @frame = frame
-
-    @layout = GridBagLayout.new
-    @panel = JPanel.new @layout
-
-    @constraints = GridBagConstraints.new
-    @constraints.fill = GridBagConstraints.BOTH
   end
 
   def call!
@@ -31,18 +28,7 @@ class ThresholdRangeService < BaseService
     add_component_with_constraints(2, 0, 1, 1) { ConfigTextField.new :threshold_min, '', MAX_TEXT_FIELD_COUNT }
     add_component_with_constraints(1, 1, 1, 1) { JLabel.new '最大値' }
     add_component_with_constraints(2, 1, 1, 1) { ConfigTextField.new :threshold_max, '', MAX_TEXT_FIELD_COUNT }
-  end
 
-  private
-
-  def add_component_with_constraints(gridx, gridy, gridwidth, gridheight, &block)
-    @constraints.gridx = gridx
-    @constraints.gridy = gridy
-    @constraints.gridwidth = gridwidth
-    @constraints.gridheight = gridheight
-
-    component = block.call
-    @layout.set_constraints component, @constraints
-    @panel.add component
+    @frame.get_content_pane.add panel
   end
 end
