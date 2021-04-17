2407354e6a852e06ad1caa9087f30460a6073ab6
masahito1997 <velonica.gale2227@gmail.com>
2021-04-17 14:59:35 +0900

    set error dialog size and adjust label alignment

--- services/exception_dialog.rb
+++ services/exception_dialog.rb
@@ -15,11 +15,14 @@ class ExceptionDialog
 
   def call!
     dialog = JDialog.new @frame
+    dialog.set_size 200, 150
+    dialog.set_location_relative_to nil
     dialog.set_title('Error Log')
+
     label = JLabel.new @error_message
+    label.set_horizontal_alignment(JLabel::CENTER)
 
     dialog.get_content_pane.add(label, 'Center')
-    dialog.pack
     dialog.set_visible(true)
   end
 end
