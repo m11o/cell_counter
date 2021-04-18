839bfbb0506c5ee62dfc13f67262d3339aef979b
masahito1997 <velonica.gale2227@gmail.com>
2021-04-18 10:59:22 +0900

    add padding

--- services/contrast_range_service.rb
+++ services/contrast_range_service.rb
@@ -5,6 +5,7 @@ require_relative "../lib/config_text_field.rb"
 require_relative "../lib/grid_bag_layout_helper.rb"
 
 java_import "javax.swing.JLabel"
+java_import "javax.swing.border.EmptyBorder"
 
 class ContrastRangeService < BaseService
   include GridBagLayoutHelper
@@ -23,6 +24,8 @@ class ContrastRangeService < BaseService
     add_component_with_constraints(0, 2, 1, 1) { JLabel.new '最大値' }
     add_component_with_constraints(1, 2, 1, 1) { ConfigTextField.new :contrast_max, '', MAX_TEXT_FIELD_COUNT }
 
+    padding = EmptyBorder.new(0, 0, 5, 0)
+    panel.set_border(padding)
     @frame.add panel
   end
 end
