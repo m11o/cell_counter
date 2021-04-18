b90e77fa0359733fb692ee1957183d6c846523a7
masahito1997 <velonica.gale2227@gmail.com>
2021-04-18 13:48:24 +0900

    remove empty border to create padding

--- services/particle_size_range_service.rb
+++ services/particle_size_range_service.rb
@@ -5,7 +5,6 @@ require_relative "../lib/config_text_field.rb"
 require_relative "../lib/grid_bag_layout_helper.rb"
 
 java_import "javax.swing.JLabel"
-java_import "javax.swing.border.EmptyBorder"
 
 class ParticleSizeRangeService
   include GridBagLayoutHelper
@@ -23,8 +22,6 @@ class ParticleSizeRangeService
     add_component_with_constraints(0, 2, 1, 1) { JLabel.new '最大値' }
     add_component_with_constraints(1, 2, 1, 1) { ConfigTextField.new :particle_size_max, '', MAX_TEXT_FIELD_COUNT }
 
-    padding = EmptyBorder.new(0, 0, 5, 0)
-    panel.set_border(padding)
     @frame.add panel
   end
 end
