43cefd92ea50931fea8debdc848a3a46c1362cbe
masahito1997 <velonica.gale2227@gmail.com>
2021-04-18 13:18:22 +0900

    implement to call for GridBagConstraints Alignment const

--- lib/grid_bag_layout_helper.rb
+++ lib/grid_bag_layout_helper.rb
@@ -20,7 +20,7 @@ module GridBagLayoutHelper
     @constraints ||= GridBagConstraints.new
   end
 
-  def add_component_with_constraints(gridx, gridy, gridwidth, gridheight, anchor = GridBagConstraints::WEST, &block)
+  def add_component_with_constraints(gridx, gridy, gridwidth, gridheight, anchor = anchor_west, &block)
     constraints.gridx = gridx
     constraints.gridy = gridy
     constraints.gridwidth = gridwidth
@@ -34,5 +34,12 @@ module GridBagLayoutHelper
 
   def build_padding_insets(top: 0, left: 0, bottom: 0, right: 0)
     Insets.new top, left, bottom, right
+  end
+
+  def method_missing(name, *args)
+    super if name !~ /\Aanchor_(.+?)\z/
+
+    const_name = Regexp.last_match[1].upcase
+    GridBagConstraints.const_get const_name.to_sym
   end
 end
